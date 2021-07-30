3dc72dec735b6d2f8805c2be1b398614e8b4ba5d
rasyidin <raflirasyidin20@gmail.com>
2021-07-30 18:58:58 +0700

    setup retrofit instance

---
+++ app/src/main/java/com/rasyidin/serieshunt/core/di/NetworkModule.kt
@@ -0,0 +1,37 @@
+package com.rasyidin.serieshunt.core.di
+
+import com.rasyidin.serieshunt.core.data.source.remote.network.ApiService
+import com.rasyidin.serieshunt.core.utils.Constants.BASE_URL
+import com.rasyidin.serieshunt.core.utils.Constants.REQUEST_TIMEOUT
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun providesHttpClient() = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .connectTimeout(REQUEST_TIMEOUT, TimeUnit.SECONDS)
+        .readTimeout(REQUEST_TIMEOUT, TimeUnit.SECONDS)
+        .build()
+
+    @Provides
+    @Singleton
+    fun providesRetrofitInstance(): ApiService = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(providesHttpClient())
+        .build()
+        .create(ApiService::class.java)
+}
--- app/src/main/java/com/rasyidin/serieshunt/core/utils/Constants.kt
+++ app/src/main/java/com/rasyidin/serieshunt/core/utils/Constants.kt
@@ -7,4 +7,6 @@ object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
 
     const val BASE_YOUTUBE_URL = "https://www.youtube.com/watch?v="
+
+    const val REQUEST_TIMEOUT = 20L
 }
