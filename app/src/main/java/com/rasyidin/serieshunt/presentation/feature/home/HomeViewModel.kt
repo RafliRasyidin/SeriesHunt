5bbb3adc2726ebeb3f577c8ad75afce8d2648663
rasyidin <raflirasyidin20@gmail.com>
2021-07-30 20:27:56 +0700

    setup home view model

--- app/src/main/java/com/rasyidin/serieshunt/presentation/feature/home/HomeViewModel.kt
+++ app/src/main/java/com/rasyidin/serieshunt/presentation/feature/home/HomeViewModel.kt
@@ -1,4 +1,21 @@
 package com.rasyidin.serieshunt.presentation.feature.home
 
-class HomeViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.rasyidin.serieshunt.core.domain.usecase.ITvShowUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class HomeViewModel @Inject constructor(tvShowUseCase: ITvShowUseCase) : ViewModel(){
+
+    val getAiringToday = tvShowUseCase.getAiringToday().asLiveData(viewModelScope.coroutineContext)
+
+    val getOnTheAir = tvShowUseCase.getOnTheAir().asLiveData(viewModelScope.coroutineContext)
+
+    val getPopular = tvShowUseCase.getPopular().asLiveData(viewModelScope.coroutineContext)
+
+    val getTopRated = tvShowUseCase.getTopRated().asLiveData(viewModelScope.coroutineContext)
+
 }
