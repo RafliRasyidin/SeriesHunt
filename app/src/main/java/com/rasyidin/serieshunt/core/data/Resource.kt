58bba1dc37745466ba6fb089ea31522923859ba2
rasyidin <raflirasyidin20@gmail.com>
2021-07-30 19:00:26 +0700

    create sealed class Resource to handle data stream state

--- app/src/main/java/com/rasyidin/serieshunt/core/data/Resource.kt
+++ app/src/main/java/com/rasyidin/serieshunt/core/data/Resource.kt
@@ -1,3 +1,7 @@
 package com.rasyidin.serieshunt.core.data
 
-sealed class Resource
+sealed class Resource<T>(val data: T?, val message: String? = null) {
+    class Success<T>(data: T?) : Resource<T>(data)
+    class Loading<T>(data: T? = null) : Resource<T>(data)
+    class Error<T>(data: T? = null, message: String) : Resource<T>(data, message)
+}
