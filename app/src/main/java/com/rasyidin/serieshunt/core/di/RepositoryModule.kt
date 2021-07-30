bf8e770b818faa8812c2d566769c263cca84a336
rasyidin <raflirasyidin20@gmail.com>
2021-07-30 20:16:46 +0700

    provides repository module

---
+++ app/src/main/java/com/rasyidin/serieshunt/core/di/RepositoryModule.kt
@@ -0,0 +1,26 @@
+package com.rasyidin.serieshunt.core.di
+
+import com.rasyidin.serieshunt.core.data.TvShowRepository
+import com.rasyidin.serieshunt.core.data.source.remote.RemoteDataSource
+import com.rasyidin.serieshunt.core.data.source.remote.network.ApiService
+import com.rasyidin.serieshunt.core.domain.repository.ITvShowRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+
+    @Provides
+    @Singleton
+    fun providesRemoteDataSource(apiService: ApiService): RemoteDataSource =
+        RemoteDataSource(apiService)
+
+    @Provides
+    @Singleton
+    fun providesTvShowRepository(remoteDataSource: RemoteDataSource): ITvShowRepository =
+        TvShowRepository(remoteDataSource)
+}
