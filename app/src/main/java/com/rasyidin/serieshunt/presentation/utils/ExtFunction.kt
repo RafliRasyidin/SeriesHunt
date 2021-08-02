b6353fdc5abd8419505cdc0a0550e5d6b7249873
rasyidin <raflirasyidin20@gmail.com>
2021-08-02 21:13:10 +0700

    create ext function to formatting date

--- app/src/main/java/com/rasyidin/serieshunt/presentation/utils/ExtFunction.kt
+++ app/src/main/java/com/rasyidin/serieshunt/presentation/utils/ExtFunction.kt
@@ -1,1 +1,12 @@
 package com.rasyidin.serieshunt.presentation.utils
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+@Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+fun String.toYearFormat(): String {
+    val inputFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+    val outputFormat = SimpleDateFormat("yyyy", Locale.getDefault())
+    val date = inputFormat.parse(this)
+    return outputFormat.format(date)
+}
