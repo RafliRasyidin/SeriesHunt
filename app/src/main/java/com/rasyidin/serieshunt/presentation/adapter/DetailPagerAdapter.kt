d420c75e78f5aad6ad10b41794c840fc08d92920
rasyidin <raflirasyidin20@gmail.com>
2021-08-04 15:24:48 +0700

    create detail pager adapter

---
+++ app/src/main/java/com/rasyidin/serieshunt/presentation/adapter/DetailPagerAdapter.kt
@@ -0,0 +1,37 @@
+package com.rasyidin.serieshunt.presentation.adapter
+
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.rasyidin.serieshunt.R
+import com.rasyidin.serieshunt.presentation.feature.detail.episodes.EpisodesFragment
+import com.rasyidin.serieshunt.presentation.feature.detail.overview.OverviewFragment
+import com.rasyidin.serieshunt.presentation.feature.detail.trailers.TrailersFragment
+
+class DetailPagerAdapter(
+    fm: FragmentManager,
+    lifecycle: Lifecycle,
+    private val idTv: Int,
+    private val overview: String?
+) : FragmentStateAdapter(fm, lifecycle) {
+
+    override fun getItemCount(): Int {
+        return TAB_TITLES.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> OverviewFragment.newInstance(idTv, overview)
+            1 -> EpisodesFragment.newInstance(idTv)
+            else -> TrailersFragment.newInstance(idTv)
+        }
+    }
+
+    companion object {
+        @StringRes
+        val TAB_TITLES = intArrayOf(R.string.overview, R.string.episodes, R.string.trailers)
+    }
+
+}
