ae5334dc68fbfb45e84e2fcb3c2a527110305345
rasyidin <raflirasyidin20@gmail.com>
2021-08-04 17:01:45 +0700

    Adding base youtube thumbnail url

--- app/src/main/java/com/rasyidin/serieshunt/core/utils/Constants.kt
+++ app/src/main/java/com/rasyidin/serieshunt/core/utils/Constants.kt
@@ -7,6 +7,10 @@ object Constants {
     const val BASE_URL = "https://api.themoviedb.org/3/"
 
     const val BASE_YOUTUBE_URL = "https://www.youtube.com/watch?v="
+    
+    const val BASE_YOUTUBE_THUMBNAIL_URL = "https://img.youtube.com/vi/"
+
+    const val BASE_YOUTUBE_THUMBNAIL_QUALITY_URL = "/0.jpg"
 
     const val REQUEST_TIMEOUT = 20L
 }
