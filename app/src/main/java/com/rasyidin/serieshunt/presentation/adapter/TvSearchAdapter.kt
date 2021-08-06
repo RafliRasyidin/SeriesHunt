5bd186fa9b6722edf9049dc1d96f816259309c60
rasyidin <raflirasyidin20@gmail.com>
2021-08-06 16:32:16 +0700

    create search adapter

---
+++ app/src/main/java/com/rasyidin/serieshunt/presentation/adapter/TvSearchAdapter.kt
@@ -0,0 +1,30 @@
+package com.rasyidin.serieshunt.presentation.adapter
+
+import com.bumptech.glide.RequestManager
+import com.rasyidin.serieshunt.R
+import com.rasyidin.serieshunt.core.domain.model.TvShow
+import com.rasyidin.serieshunt.core.utils.Constants.BASE_URL_IMAGE
+import com.rasyidin.serieshunt.databinding.ItemSearchBinding
+import com.rasyidin.serieshunt.presentation.utils.toOnlyYearFormat
+import javax.inject.Inject
+
+class TvSearchAdapter @Inject constructor(private val glide: RequestManager) :
+    BaseAdapter<TvShow>(R.layout.item_search, TvShowAdapter.DiffCallback) {
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val tvShow = getItem(position)
+        val binding = ItemSearchBinding.bind(holder.itemView)
+        with(binding) {
+            glide.load(BASE_URL_IMAGE + tvShow.posterPath)
+                .placeholder(R.drawable.ic_tv_placeholder)
+                .into(imgPoster)
+            tvTitle.text = tvShow.name
+            tvDate.text = tvShow.firstAirDate?.toOnlyYearFormat()
+
+            root.setOnClickListener {
+                onItemClick?.invoke(tvShow)
+            }
+        }
+    }
+
+}
