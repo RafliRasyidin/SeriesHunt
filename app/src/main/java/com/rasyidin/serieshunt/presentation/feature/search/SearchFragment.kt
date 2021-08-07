cee7d235cbeec31cd596ce38d66940d0ea1e84ac
rasyidin <raflirasyidin20@gmail.com>
2021-08-07 19:41:39 +0700

    observe search tv show

--- app/src/main/java/com/rasyidin/serieshunt/presentation/feature/search/SearchFragment.kt
+++ app/src/main/java/com/rasyidin/serieshunt/presentation/feature/search/SearchFragment.kt
@@ -2,14 +2,101 @@ package com.rasyidin.serieshunt.presentation.feature.search
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.rasyidin.serieshunt.R
+import com.rasyidin.serieshunt.core.data.Resource
 import com.rasyidin.serieshunt.databinding.FragmentSearchBinding
+import com.rasyidin.serieshunt.presentation.adapter.TvSearchAdapter
 import com.rasyidin.serieshunt.presentation.base.BaseFragment
+import com.rasyidin.serieshunt.presentation.feature.detail.DetailContentFragment.Companion.ARG_OVERVIEW
+import com.rasyidin.serieshunt.presentation.feature.detail.DetailContentFragment.Companion.ARG_TV_ID
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@ExperimentalCoroutinesApi
+@FlowPreview
+@ObsoleteCoroutinesApi
+@AndroidEntryPoint
 class SearchFragment : BaseFragment<FragmentSearchBinding>(FragmentSearchBinding::inflate) {
+    
+    private val viewModel: SearchViewModel by viewModels()
+    
+    @Inject
+    lateinit var tvSearchAdapter: TvSearchAdapter
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        
+        setupRv()
+        
+        searchTvShow()
 
+        observeSearchTv()
+
+        navigateToDetail()
+    }
+
+    private fun navigateToDetail() {
+        tvSearchAdapter.onItemClick = { tvShow ->
+            val args = Bundle().apply {
+                putInt(ARG_TV_ID, tvShow.id)
+                putString(ARG_OVERVIEW, tvShow.overview)
+            }
+            findNavController().navigate(R.id.action_searchFragment_to_detailContentFragment, args)
+        }
+    }
+
+    private fun searchTvShow() {
+        binding.svTv.onActionViewExpanded()
+        binding.svTv.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                lifecycleScope.launch {
+                    viewModel.queryChannel.send(newText)
+                }
+                return true
+            }
+        })
+    }
+
+    private fun observeSearchTv() {
+        viewModel.searchTvShow.observe(viewLifecycleOwner) { tvShow ->
+            tvShow.observe(viewLifecycleOwner) { resource ->
+                when (resource) {
+                    is Resource.Error -> Toast.makeText(activity, "Something Wrong!", Toast.LENGTH_SHORT).show()
+                    is Resource.Loading -> Unit
+                    is Resource.Success -> {
+                        val data = resource.data
+                        data?.let {
+                            if (data.isNotEmpty()) {
+                                tvSearchAdapter.submitList(data)
+                            } else {
+                                Toast.makeText(activity, "Not Found!", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+
+                    }
+                }
+            }
+
+        }
+    }
+
+    private fun setupRv() = binding.rvSearch.apply { 
+        adapter = tvSearchAdapter
+        layoutManager = LinearLayoutManager(activity)
     }
 
 }
--- app/src/main/java/com/rasyidin/serieshunt/presentation/feature/search/SearchViewModel.kt
+++ app/src/main/java/com/rasyidin/serieshunt/presentation/feature/search/SearchViewModel.kt
@@ -1,4 +1,32 @@
 package com.rasyidin.serieshunt.presentation.feature.search
 
-class SearchViewModel {
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.rasyidin.serieshunt.core.domain.usecase.ITvShowUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.channels.BroadcastChannel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.*
+import javax.inject.Inject
+
+@ObsoleteCoroutinesApi
+@FlowPreview
+@ExperimentalCoroutinesApi
+@HiltViewModel
+class SearchViewModel @Inject constructor(private val useCase: ITvShowUseCase) : ViewModel() {
+
+    val queryChannel = BroadcastChannel<String>(Channel.CONFLATED)
+
+    val searchTvShow = queryChannel.asFlow()
+        .debounce(1000)
+        .distinctUntilChanged()
+        .filter {
+            it.trim().isNotEmpty()
+        }.mapLatest { query ->
+            useCase.searchTvShow(query).asLiveData(viewModelScope.coroutineContext)
+        }.asLiveData(viewModelScope.coroutineContext)
 }
