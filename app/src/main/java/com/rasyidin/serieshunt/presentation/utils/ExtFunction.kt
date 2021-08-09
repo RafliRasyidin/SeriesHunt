72dcde9a1b4331e998ba9e1d94600721f940ab23
rasyidin <raflirasyidin20@gmail.com>
2021-08-09 15:00:13 +0700

    refactor code

--- app/src/main/java/com/rasyidin/serieshunt/presentation/utils/ExtFunction.kt
+++ app/src/main/java/com/rasyidin/serieshunt/presentation/utils/ExtFunction.kt
@@ -5,10 +5,15 @@ import java.util.*
 
 @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 fun String.toOnlyYearFormat(): String {
-    val inputFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-    val outputFormat = SimpleDateFormat("yyyy", Locale.getDefault())
-    val date = inputFormat.parse(this)
-    return outputFormat.format(date)
+    return if (this.isEmpty()) {
+        this
+    } else {
+        val inputFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+        val outputFormat = SimpleDateFormat("yyyy", Locale.getDefault())
+        val date = inputFormat.parse(this)
+        outputFormat.format(date)
+    }
+
 }
 
 @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
