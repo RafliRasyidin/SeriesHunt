993d4cf3579071b03fb63b6d00fa3275c4c42322
rasyidin <raflirasyidin20@gmail.com>
2021-08-11 11:18:54 +0700

    Refactor Code using View Binding

--- app/src/main/java/com/rasyidin/serieshunt/presentation/adapter/BaseAdapter.kt
+++ app/src/main/java/com/rasyidin/serieshunt/presentation/adapter/BaseAdapter.kt
@@ -3,23 +3,25 @@ package com.rasyidin.serieshunt.presentation.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.annotation.LayoutRes
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
 
-abstract class BaseAdapter<T>(
-    @LayoutRes private val layoutId: Int,
+typealias Inflate<T> = (LayoutInflater, ViewGroup?, Boolean) -> T
+abstract class BaseAdapter<T, VB: ViewBinding>(
+    private val inflate: Inflate<VB>,
     diffCallback: DiffUtil.ItemCallback<T>
-) :
-    ListAdapter<T, BaseAdapter.ItemViewHolder>(diffCallback) {
+) : ListAdapter<T, BaseAdapter.ItemViewHolder>(diffCallback) {
 
-    class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    class ItemViewHolder(binding: ViewBinding) : RecyclerView.ViewHolder(binding.root)
 
-    var onItemClick: ((T) -> Unit)? = null
+    var onItemClick: ((T, view1: View?, view2: View?) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        return ItemViewHolder(LayoutInflater.from(parent.context).inflate(layoutId, parent, false))
+        return ItemViewHolder(inflate.invoke(LayoutInflater.from(parent.context), parent, false))
     }
+
+
 
 }
